b00624019306af888a1b8f0f5ac6fa7e77ddae46
Dan Tenenbaum <dtenenba@fhcrc.org>
2018-04-09 16:31:23 -0700

    part of the way there

--- main.go
+++ main.go
@@ -30,6 +30,12 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/google/uuid"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	tagOnly = kingpin.Flag("tag-only", "Just tag, don't upload").Short('t').Bool()
+	csvFile = kingpin.Arg("csv", "CSV file").Required().String()
 )
 
 // DataType determines whether this is array or sequencing data
@@ -182,40 +188,137 @@ func uploadFile(fileToUpload string, rec CSVRecord, wg *sync.WaitGroup, svc s3.S
 
 }
 
+//
+// func tagFile(fileToUpload string, rec CSVRecord, wg *sync.WaitGroup, svc s3.S3, ops *uint64, guard chan struct{}) {
+// 	defer wg.Done()
+// 	defer func() { <-guard }()
+// 	if fileExistsInS3(fileToUpload, rec, svc) {
+// 		fmt.Println(rec.s3Prefix+fileToUpload, "already exists in S3; not overwriting.")
+// 		return
+// 	}
+// 	st, err := os.Lstat(rec.localDir)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	var fd *os.File
+// 	if st.IsDir() {
+// 		fd, err = os.Open(rec.localDir + fileToUpload)
+// 	} else {
+// 		fd, err = os.Open(rec.localDir)
+// 	}
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer fd.Close()
+// 	v := url.Values{}
+// 	v.Set("stage", rec.stage)
+// 	v.Set("assay_material_id", rec.assayMaterialID)
+// 	v.Set("molecular_id", rec.molecularID)
+// 	v.Set("omics_sample_name", rec.omicsSampleName)
+//
+// 	// FIXME add omicsSampleName here?
+// 	input := &s3.PutObjectInput{
+// 		Bucket:  aws.String(rec.s3TransferBucket),
+// 		Key:     aws.String(rec.s3Prefix + fileToUpload),
+// 		Body:    fd,
+// 		Tagging: aws.String(v.Encode()),
+// 	}
+// 	_, perr := svc.PutObject(input)
+// 	if perr != nil {
+// 		if aerr, ok := perr.(awserr.Error); ok {
+// 			switch aerr.Code() {
+// 			default:
+// 				fmt.Println(aerr.Error())
+// 			}
+// 		} else {
+// 			// Print the error, cast err to awserr.Error to get the Code and
+// 			// Message from an error.
+// 			fmt.Println(perr.Error())
+// 		}
+// 		// return
+// 	}
+// 	atomic.AddUint64(ops, 1)
+// 	fmt.Printf("Uploading %s%s with tags %s...\n", rec.s3Prefix, fileToUpload, v.Encode())
+//
+// }
+
+//
+
 func handleRecord(record []string, wg *sync.WaitGroup, svc s3.S3, cmd string, ops *uint64, guard chan struct{}) {
 	rec := CSVRecord{molecularID: record[3], assayMaterialID: record[4],
 		s3TransferBucket: record[1], s3Prefix: record[2], localDir: record[0],
 		stage: record[5], omicsSampleName: record[6],
 		dataType: DataType(strings.TrimSpace(record[7]))}
 
-	if !strings.HasSuffix(rec.s3Prefix, "/") {
-		rec.s3Prefix = rec.s3Prefix + "/"
-	}
-
-	f, err := os.Lstat(rec.localDir)
-	if err != nil {
-		panic(err)
-	}
-
-	var uploadSingleFile bool
+	var file os.FileInfo
 
 	var files []os.FileInfo
 
-	if f.IsDir() {
-		if !strings.HasSuffix(rec.localDir, "/") {
-			rec.localDir = rec.localDir + "/"
-		}
+	var uploadSingleFile bool
 
-		files, err = ioutil.ReadDir(rec.localDir)
+	if cmd == "tag" {
+		// first, see if prefix refers to an object. if so, we are only tagging that object.
+		// otherwise we are tagging all eligible objects that start with prefix.
+
+		// input := &s3.ListObjectsV2Input{
+		// 	Bucket: aws.String(rec.s3TransferBucket),
+		// 	// Delimiter: aws.String("/"),
+		// 	Prefix: aws.String(rec.s3Prefix),
+		// }
+		// res, err := svc.ListObjectsV2(input)
+		// if err != nil {
+		// 	panic(err)
+		// }
+		// fmt.Println("# returned:", len(res.Contents))
+		input := &s3.HeadObjectInput{
+			Bucket: aws.String(rec.s3TransferBucket),
+			Key:    aws.String(rec.s3Prefix),
+		}
+		out, err := svc.HeadObject(input)
+		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok {
+				switch aerr.Code() {
+				default:
+					fmt.Println(aerr.Error())
+				}
+			} else {
+
+				if !strings.HasSuffix(rec.s3Prefix, "/") {
+					rec.s3Prefix = rec.s3Prefix + "/"
+				}
+
+				// Print the error, cast err to awserr.Error to get the Code and
+				// Message from an error.
+				fmt.Println(err.Error())
+			}
+		}
+		fmt.Println("input is", input)
+		fmt.Println("result is", out)
+		os.Exit(1)
+
+	} else {
+		f, err := os.Lstat(rec.localDir)
 		if err != nil {
 			panic(err)
 		}
-	} else {
-		uploadSingleFile = true
-		files = append(files, f)
+
+		if f.IsDir() {
+			if !strings.HasSuffix(rec.localDir, "/") {
+				rec.localDir = rec.localDir + "/"
+			}
+
+			files, err = ioutil.ReadDir(rec.localDir)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			uploadSingleFile = true
+			files = append(files, f)
+		}
+
 	}
 
-	for _, file := range files {
+	for _, file = range files {
 		if file.IsDir() {
 			continue
 		}
@@ -228,11 +331,18 @@ func handleRecord(record []string, wg *sync.WaitGroup, svc s3.S3, cmd string, op
 				continue
 			}
 		}
+
+		// }
+
 		switch cmd {
 		case "upload":
 			wg.Add(1)
 			guard <- struct{}{}
 			go uploadFile(file.Name(), rec, wg, svc, ops, guard)
+		// case "tag":
+		// 	wg.Add(1)
+		// 	guard <- struct{}{}
+		// 	go tagFile(rec, wg, svc, ops, guard)
 		case "delete":
 			deleteFile(file.Name(), rec, wg, svc, ops)
 		case "count":
@@ -242,13 +352,7 @@ func handleRecord(record []string, wg *sync.WaitGroup, svc s3.S3, cmd string, op
 }
 
 func main() {
-
-	var ops uint64
-	maxGoroutines := runtime.NumCPU() - 1
-	guard := make(chan struct{}, maxGoroutines)
-
-	if len(os.Args) < 2 {
-		fmt.Printf(`
+	kingpin.CommandLine.Help = `
 Please supply the name of a csv file containing the following a header line
 like this:
 
@@ -261,17 +365,25 @@ https://teams.fhcrc.org/sites/citwiki/SciComp/Pages/Grab%%20Commands.aspx
 
 Full documentation is here:
 https://github.com/FredHutch/s3tagcrawler/tree/feature/uploadAndTag
-`)
-		os.Exit(1)
-	}
+
+`
+	kingpin.Parse()
+
+	var ops uint64
+	maxGoroutines := runtime.NumCPU() - 1
+	guard := make(chan struct{}, maxGoroutines)
+
 	cmd := "upload"
-	if len(os.Args) > 2 {
-		cmd = os.Args[2]
+	// if len(os.Args) > 2 {
+	// 	cmd = os.Args[2]
+	// }
+	if *tagOnly {
+		cmd = "tag"
 	}
 	os.Setenv("AWS_REGION", "us-west-2") // FIXME
 	svc := s3.New(session.New())
 
-	fileHandle, err := os.Open(os.Args[1])
+	fileHandle, err := os.Open(*csvFile)
 	if err != nil {
 		panic(err)
 	}
